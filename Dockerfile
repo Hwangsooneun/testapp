3fa75aeec99880d330bdd5cb74849317c2dbd3fc
Hwangsooneun <catalyst8849@gmail.com>
2022-02-19 18:44:12 +0900

    Test: 멀터 테스트

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
-FROM 190594267513.dkr.ecr.ap-northeast-2.amazonaws.com/node:latest
-
-WORKDIR /home/app
-COPY package*.json ./
-RUN npm install
-
+FROM --platform=amd64 190594267513.dkr.ecr.ap-northeast-2.amazonaws.com/node:latest
+WORKDIR /usr/src/app
 COPY . .
 
+RUN npm install
 CMD node index.js
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const bcrypt = require("bcrypt")
-const app = require("express")();
+const upload = require("./upload");
+const express = require("express")
+const { urlencoded } = require('express');
+const app = express()
+const http = require('http')
+const Stream = require('stream').Transform;
+const fs = require('fs');
+const dotenv = require('dotenv')
+dotenv.config()
+
+app.use(express.json())
+app.use(urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Success!! and Docker test for ec2",
+    message: process.env.TEST,
   });
 });
 
@@ -13,6 +23,28 @@ app.get("/hello", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.post("/multer", upload.single('img'), (req, res) => {
+  const image = req.file.location;
+  console.log(image)
+})
+
+/* request body: { "img": "http://postfiles.pstatic.net/MjAyMjAyMTRfMTMg/MDAxNjQ0ODI0NTAyMTkz.ObOk294g3V53BRiAQnzK8c536a2FCJxeKbvD_txcdowg.Af0cOVeKt-Nk-qCMoGLajManffDbL4zHmbpEt7PODNYg.PNG.catalyst88/%EC%98%A4%EB%A0%8C%EC%A7%80%EB%B3%B4%EB%93%9C-logo(%EC%83%81%ED%95%98)_ccexpress.png?type=w773" } */
+app.post("/download", (req, res) => {
+  console.log(req.body.img)
+  var url = req.body.img;
+
+  http.request(url, function (response) {
+    var data = new Stream();
+
+    response.on('data', function (chunk) {
+      data.push(chunk);
+    });
+    response.on('end', function (c) {
+      fs.writeFileSync('image.png', data.read());
+    });
+  }).end();
+})
+
+app.listen(80, () => {
   console.log("Server starting on port 80");
 });
